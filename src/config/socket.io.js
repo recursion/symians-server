9640ec6a4d5e447b0864aa34377e6ae47aad21b7
Michael Symmes <msymmes@gmail.com>
2015-10-17 15:50:13 -0400

    Updated for libs and redis models integrations

--- src/config/socket.io.js
+++ src/config/socket.io.js
@@ -2,8 +2,37 @@ import http from 'http'
 import socketio from 'socket.io'
 import socketServices from '../sockets'
 import winston from 'winston'
+import {loader} from 'symians-models'
 
-let world;
+const world = {};
+world.zone = {};
+
+let ready = false;
+let length = 0;
+
+/**
+ * copy data from one object
+ *  onto our world.zone
+ */
+function setZone(z){
+  world.zone.height = z.height;
+  world.zone.width = z.width;
+  world.zone.locations = z.locations.slice();
+}
+
+/**
+ * inflate the existing zone
+ */
+loader.inflateZone(1)
+  .then(function(z){
+    console.log(z.locations.length);
+    length = z.locations.length;
+    setZone(z);
+    ready = true;
+  })
+  .catch((err)=>{
+    throw new Error(err);
+  });
 
 const port = process.env.PORT || 3000;
 
@@ -19,7 +48,6 @@ export default function (app){
   // create a socket.io server wrapped around the httpServer
   const io = socketio(httpServer);
 
-
   // socket event handlers
   io.on('connection', (socket)=> {
 
@@ -28,14 +56,15 @@ export default function (app){
     /**
      * send the full world for the initial connection
      */
-    socket.emit('world-init', world);
+    socket.emit('zone-init', world.zone);
 
     /**
      * once the client has loaded the world
      * subscribe them to updates
      */
-    socket.on('world-loaded', ()=> {
-      socket.join('world-updates');
+    socket.on('zone-loaded', ()=> {
+      // change this to join the zoneID channel
+      socket.join('zone-updates');
     });
 
     /**
@@ -59,9 +88,12 @@ export default function (app){
   // TODO: optimize this in all sorts of ways.
   // we shouldnt really ever need to send the entire map
   // after the initial connection.
+
+  /* dont need right now
   setInterval(()=> {
     io.to('world-updates').emit('world-update', world);
   }, 10000);
+  */
 
   // start listening
   httpServer.listen(port, ()=> {
